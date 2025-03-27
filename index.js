295584ce2f30861ff77e9a8bab46ccff9fad79c4
alasiri <austinalasiri44@gmail.com>
2025-03-27 20:42:20 +0300

    Finished the tests

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let revertedString = '';
+  for(let i = word.length-1; i >= 0; i--){
+      revertedString += word[i];
+  }
+
+  if(revertedString === word){
+    return true;
+  }else{
+    return false;
+  }
+  
 }
 
 /* 
   Add your pseudocode here
+  Create a variable of type string with let keyword and assign it to an empty string
+  Create a for loop to iterate through the input, let it be of a descending format
+  Add each element of the input(now from last index to start index) to the variable
+  If variable is equal to input return true, else return false
+
 */
 
 /*
@@ -14,12 +30,12 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome('madam'));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome('rayana'));
 }
 
 module.exports = isPalindrome;
